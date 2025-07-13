7adefcff8d81c77dbe2728adeccafaca39776a80
Kazakov Alexey <admin@kazakov-a.ru>
2025-07-13 22:48:07 +0300

    some

--- 03-3d-transformation/module-work/pre/scripts/script.js
+++
@@ -1,8 +0,0 @@
-const obj = document.querySelector(".cube");
-const ranges = document.querySelectorAll(".range");
-
-ranges.forEach((range) => {
-  range.addEventListener("input", () => {
-    obj.style.setProperty(`--${range.name}`, range.value + "deg");
-  });
-});
